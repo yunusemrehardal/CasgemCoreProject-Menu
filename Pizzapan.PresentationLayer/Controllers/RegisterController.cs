3b0013008ca7e8a24ef3153124afd69911b9986f
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-07-17 15:09:44 +0300

    E-mail Confirmed

--- Pizzapan.PresentationLayer/Controllers/RegisterController.cs
+++ Pizzapan.PresentationLayer/Controllers/RegisterController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 using Pizzapan.EntityLayer.Concrete;
 using Pizzapan.PresentationLayer.Models;
+using System;
 using System.Threading.Tasks;
 
 namespace Pizzapan.PresentationLayer.Controllers
 {
     public class RegisterController : Controller
     {
+
         private readonly UserManager<AppUser> _userManager;
 
         public RegisterController(UserManager<AppUser> userManager)
@@ -23,19 +27,44 @@ namespace Pizzapan.PresentationLayer.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(RegisterViewModel model)
         {
+            Random rnd = new Random();
+            int x = rnd.Next(100000, 1000000);
+
             AppUser appUser = new AppUser()
             {
                 Name = model.Name,
                 Surname = model.SurName,
                 Email = model.Email,
-                UserName = model.UserName
+                UserName = model.UserName,
+                ConfirmCode =x
             };
             if (model.Password == model.ConfirmPassword)
             {
                 var result = await _userManager.CreateAsync(appUser, model.Password);
+
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Login");
+                    #region
+                    MimeMessage mimeMessage = new MimeMessage();
+                    MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "hardaalyunusemre@gmail.com");
+                    mimeMessage.From.Add(mailboxAddressFrom);
+
+                    MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.Email);
+                    mimeMessage.To.Add(mailboxAddressTo);
+
+                    var bodyBuilder = new BodyBuilder();
+                    bodyBuilder.TextBody = "Giriş yapabilmek için onay kodunuz:" + x;
+                    mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+                    SmtpClient smtpcilent = new SmtpClient();
+                    smtpcilent.Connect("smtp.gmail.com", 587, false);
+                    smtpcilent.Authenticate("hardaalyunusemre@gmail.com", "vjorucsdznkgqdno");
+                    smtpcilent.Send(mimeMessage);
+                    smtpcilent.Disconnect(true);
+
+                    #endregion
+                    TempData["Username"] = appUser.UserName;
+                    return RedirectToAction("Index", "ConfirmEmail");
                 }
                 else
                 {
---
+++ Pizzapan.PresentationLayer/Controllers/SendMailController.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Pizzapan.PresentationLayer.Models;
+
+namespace Pizzapan.PresentationLayer.Controllers
+{
+    public class SendMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "hardaalyunusemre@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.RecieverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.MessageContent;
+            mimeMessage.Body=bodyBuilder.ToMessageBody();
+
+            SmtpClient smtpcilent = new SmtpClient();
+            smtpcilent.Connect("smtp.gmail.com", 587, false);
+            smtpcilent.Authenticate("hardaalyunusemre@gmail.com", "vjorucsdznkgqdno");
+            smtpcilent.Send(mimeMessage);
+            smtpcilent.Disconnect(true);
+
+            return View();
+        }
+    }
+}
